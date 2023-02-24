d9f36ea7b2e4912d724e0c3324367f68e4afb304
daniel tea <daniel.l.tea@gmail.com>
2023-02-23 21:04:08 -0800

    update fontawesome icons

--- components/Footer.js
+++ components/Footer.js
@@ -1,40 +1,66 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+import {
+  faCodepen,
+  faGithub,
+  faLinkedin,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons'
+
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
 const Footer = () => {
   return (
     <footer className="site-footer">
       <nav className="site-footer-nav">
         <ul className="social-media-list">
           <li className="social-media-item">
-            <a href="https://github.com/dlcoffee" title="github">
-              <FontAwesomeIcon icon={['fab', 'github']} size="2x" />
-            </a>
-          </li>
-
-          <li className="social-media-item">
-            <a href="https://twitter.com/_dlcoffee" title="twitter">
-              <FontAwesomeIcon icon={['fab', 'twitter']} size="2x" />
-            </a>
-          </li>
-
-          <li className="social-media-item">
-            <a href="https://codepen.io/dlcoffee" title="codepen">
-              <FontAwesomeIcon icon={['fab', 'codepen']} size="2x" />
-            </a>
-          </li>
-
-          <li className="social-media-item">
-            <a href="mailto:daniel.l.tea@gmail.com" title="email">
-              <FontAwesomeIcon icon={'envelope'} size="2x" />
+            <a
+              className="social-media-item-link"
+              href="https://github.com/dlcoffee"
+              title="github"
+            >
+              <FontAwesomeIcon icon={faGithub} />
             </a>
           </li>
 
           <li className="social-media-item">
             <a
+              className="social-media-item-link"
+              href="https://twitter.com/_dlcoffee"
+              title="twitter"
+            >
+              <FontAwesomeIcon icon={faTwitter} size="2x" />
+            </a>
+          </li>
+
+          <li className="social-media-item">
+            <a
+              className="social-media-item-link"
+              href="https://codepen.io/dlcoffee"
+              title="codepen"
+            >
+              <FontAwesomeIcon icon={faCodepen} size="2x" />
+            </a>
+          </li>
+
+          <li className="social-media-item">
+            <a
+              className="social-media-item-link"
+              href="mailto:daniel.l.tea@gmail.com"
+              title="email"
+            >
+              <FontAwesomeIcon icon={faEnvelope} size="2x" />
+            </a>
+          </li>
+
+          <li className="social-media-item">
+            <a
+              className="social-media-item-link"
               href="https://linkedin.com/in/daniel-tea-1ab14697"
               title="linkedin"
             >
-              <FontAwesomeIcon icon={['fab', 'linkedin']} size="2x" />
+              <FontAwesomeIcon icon={faLinkedin} size="2x" />
             </a>
           </li>
         </ul>
