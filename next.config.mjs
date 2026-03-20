68bfab02edaab971da2c1cdac00e5660bc8b06bf
daniel tea <daniel.l.tea@gmail.com>
2026-03-19 18:14:37 -0700

    Upgrade app to Next 16 and Node 24

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 import createMDX from '@next/mdx'
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-}
+const nextConfig = {}
 
 const withMDX = createMDX({
   options: {},
